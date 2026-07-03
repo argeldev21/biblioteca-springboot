d2b7afe5bc5dc202f1156747e4341296a96274e9
argeldev21 <argeldev21@gmail.com>
2026-07-03 08:42:35 +0200

    init

---
+++ src/main/java/com/clouddev/bibliotecaspringboot/BibliotecaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.clouddev.bibliotecaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clouddev/bibliotecaspringboot/BibliotecaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddev.bibliotecaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
